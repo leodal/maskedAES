f9eed944a7ce3c1c26ab3756b89d259a0778f8f8
Léonard Dallot <leonard@dallot.net>
2013-11-06 22:00:33 +0100

    Le début : les opérations dans gf256

---
+++ Makefile
@@ -0,0 +1,13 @@
+lCC=gcc
+CFLAGS=-Wall
+
+all: test
+
+test: gf256.o secureOps.o test.o
+	$(CC) $^ -o $@
+
+run_test: test
+	./test
+
+clean:
+	-rm -f *~ *.o test
---
+++ gf256.h
@@ -0,0 +1,16 @@
+#ifndef __FIELD__
+#define __FIELD__
+
+typedef unsigned char byte;
+typedef byte byte;
+
+byte add(byte a, byte b);
+byte mult(byte a, byte b);
+byte mult_log(byte a, byte b);
+byte divide(byte a, byte b);
+byte inverse(byte a);
+byte square(byte a);
+byte pow4(byte a);
+byte pow16(byte a);
+byte pow254(byte a);
+#endif
