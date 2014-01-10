8e5bd969de74119f3c9ce69e9021e143a74d49dd
Léonard Dallot <leonard@dallot.net>
2014-01-10 14:21:01 +0100

    Commentaires dans sbox_tools

--- sbox_tools.h
+++ sbox_tools.h
@@ -1,24 +1,41 @@
+/* Transformations de Sbox de 256 octets (type byte défini dans gf256) */
+
+/* Permet de passer de leur forme standard (tables de lookup) à leur
+   forme calculatoire, par l'interpollation de Lagrange tel que décrit
+   dans la contre-mesure de Carlet et al. */
 #ifndef __SBOX_TOOLS__
 #define __SBOX_TOOLS__
 
 #include "gf256.h"
 
-void printSbox(byte polynomial[256]);
-void printPolySbox(byte sbox[256]);
-
+/* Transformation d'une sbox de sa forme polynomiale vers sa forme standard (lookup), résultat dans le paramètre "sbox" */
 void buildSbox(byte polynomial[256], byte sbox[256]);
+
+/* Transformation d'une sbox de sa forme standard (lookup) vers  sa forme polynomiale, résultat dans le paramètre "polySbox" */
 void buildPolySbox(byte sbox[256], byte polySbox[256]);
-/**
- * poly1  : polynome de degré d-1
- * poly2  : polynome de degré 1
- * result : polynome de degré d, produit de poly1 et poly2
- * Attention à l'odre : ici les petits degrés on les petits indices ; je considère l'inverse dans mes sbox
+
+/*
+  Produit de polymômes
+ 
+  poly1  : polynome de degré d-1
+  poly2  : polynome de degré 1
+  result : polynome de degré d, produit de poly1 et poly2
+  Attention à l'odre : ici les petits degrés on les petits indices ; je considère l'inverse dans mes sbox
  */
 void polyProduct(byte poly1[], byte poly2[], byte result[], int d);
+/* Multiplication d'un polynôme par un scalaire */
 void polyScalMult(byte poly[], byte a, int d);
+/* Addition de deux polynômes de même degré d */
 void polyAdd(byte poly1[], byte poly2[], byte result[], int d);
+/* Evaluation d'une sbox sous forme polynomiale */
 byte evalPolySbox(byte polynomial[256], byte x);
+/* Calcule le polynome d'interpollation de Lagrange d'une table */
 void lagrange(byte table[], byte result[], int d);
+/* Inverse un table de length élements */
 void revertTab(byte tab[], int length);
+
+/* Transformation des sbox d'une forme à l'autre, puis affichage (sous une forme acceptable par mes programmes */
+void printSbox(byte polynomial[256]);
+void printPolySbox(byte sbox[256]);
 #endif
 
