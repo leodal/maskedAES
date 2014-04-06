72a1f614bbfeee80b4e21c013c6c834060b9e77f
Léonard Dallot <leonard.dallot@taztag.com>
2014-04-06 18:28:17 +0200

    Ajout de tests pour secureAESlike et corrections

--- secureAESlike.c
+++ secureAESlike.c
@@ -5,26 +5,27 @@
 #endif
 
 /* Expanded matrix size = LINEAR_SIZE*LINEAR_SIZE*SHARES
+   Sbox size = 256*SHARES
    Clear data shares size = LINEAR_SIZE*SHARES
    Cypher data shares size = LINEAR_SIZE*SHARES
-   Temporary datas = SHARES
+   Temporary datas = 3 * SHARES
  */
-#define WSIZE LINEAR_SIZE*LINEAR_SIZE*SHARES + 256 + 2*LINEAR_SIZE*SHARES+3*SHARES + LINEAR_SIZE*NB_ROUNDS*SHARES
-byte workZone[WSIZE];
+#define WSIZE LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + 2*LINEAR_SIZE*SHARES+3*SHARES + LINEAR_SIZE*(NB_ROUNDS+1)*SHARES
+byte workZone_secAesLike[WSIZE];
 
-#define MATRIX_ADDR workZone
-#define SBOX_ADDR   (workZone + LINEAR_SIZE*LINEAR_SIZE*SHARES)
-#define KEY_ADDR    (workZone + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES)
-#define CT_ADDR     (workZone + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*NB_ROUNDS*SHARES)
-#define ET_ADDR     (workZone + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*NB_ROUNDS*SHARES + LINEAR_SIZE*SHARES)
-#define TWZ_ADDR    (workZone + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*NB_ROUNDS*SHARES + 2*LINEAR_SIZE*SHARES)
+#define MATRIX_ADDR workZone_secAesLike
+#define SBOX_ADDR   (workZone_secAesLike + LINEAR_SIZE*LINEAR_SIZE*SHARES)
+#define KEY_ADDR    (workZone_secAesLike + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES)
+#define CT_ADDR     (workZone_secAesLike + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*(NB_ROUNDS+1)*SHARES)
+#define ET_ADDR     (workZone_secAesLike + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*(NB_ROUNDS+1)*SHARES + LINEAR_SIZE*SHARES)
+#define TWZ_ADDR    (workZone_secAesLike + LINEAR_SIZE*LINEAR_SIZE*SHARES + 256*SHARES + LINEAR_SIZE*(NB_ROUNDS+1)*SHARES + 2*LINEAR_SIZE*SHARES)
 
-#ifdef DEBUG
 #include <stdio.h>
+#ifdef DEBUG
 void zeroWorkZone() {
   int j;
   for(j = 0; j < WSIZE; j++) {
-    workZone[j] = 0;
+    workZone_secAesLike[j] = 0;
   }
 }
 
@@ -32,7 +33,7 @@ void displayWorkZone() {
   int j;
   printf("##################################\n");
   for(j = 1; j < WSIZE+1; j++) {
-    printf("%#2.2x ", workZone[j-1]);
+    printf("%#2.2x ", workZone_secAesLike[j-1]);
     if(j>0 && j % 12 == 0) printf("\n");
   }
   if(j-1 % 12) printf("\n");
@@ -56,13 +57,13 @@ void secSbox(byte x[SHARES]) {
   }
 }
 
-void apply_sbox() {
+void apply_sbox(byte* addr) {
   int i;
 #ifdef DEBUG
   printf("Application de la Sbox...\n");
 #endif
   for(i = 0; i < LINEAR_SIZE; i++)
-    secSbox(CT_ADDR+i*SHARES);
+    secSbox(addr+i*SHARES);
 #ifdef DEBUG
   displayWorkZone();
 #endif
@@ -96,48 +97,34 @@ void setKey(byte key[LINEAR_SIZE*(NB_ROUNDS+1)]) {
   }
 }
 
-void matrix_product() {
+void matrix_product(byte * vect, byte * result) {
   int i,j;
 #ifdef DEBUG
-  printf("Application linéaire...\n");
+printf("Application linéaire :\n");
+  for(i = 0; i < LINEAR_SIZE; i++) {
+    printf("[ ");
+    for(j = 0; j < LINEAR_SIZE; j++) {
+      printf("%#2.2x ", collapse(MATRIX_ADDR+(i*LINEAR_SIZE+j)*SHARES));
+    }
+    printf("]\n");
+  printf("Vecteur : [");
+  for(j = 0; j < LINEAR_SIZE; j++) {
+    printf("%#2.2x ", collapse(vect+j*SHARES));
+  }
+  printf("]\n");
 #endif
-  /*#ifdef DEBUG
-  byte m, x, re, r;
-  #endif*/
   for(i = 0; i < LINEAR_SIZE; i++) {
     /* On remet à zero la zone de stockage du résultat */
     for(j = 0; j < SHARES; j++)
-      (ET_ADDR + i*SHARES)[j] = 0;
+      (result + i*SHARES)[j] = 0;
     for(j = 0; j < LINEAR_SIZE; j++) {
       /* On effecture les produits du jème elt de la ligne i de la matrice
 	 avec le jème element du vecteur */
       secMult(MATRIX_ADDR+(i*LINEAR_SIZE+j)*SHARES,
-	      CT_ADDR+j*SHARES,
+	      vect+j*SHARES,
 	      TWZ_ADDR);
-      /*#ifdef DEBUG
-      m = collapse(MATRIX_ADDR+(i*LINEAR_SIZE+j)*SHARES);
-      x = collapse(CT_ADDR+j*SHARES);
-      re = mult(m, x);
-      r = collapse(TWZ_ADDR);
-      printf("Produit %d %d : %#2.2x * %#2.2x = %#02.2x =?= %#02.2x ... %s\n",
-	     i, j,
-	     m, x, re,
-	     r, (r == re ?"OK":"KO")
-	     );
-      displayWorkZone();
-      x = collapse(ET_ADDR+j*SHARES);
-      #endif*/
-      /* On ajoute ce produit au total dans ET_ADDR[i] */
-       secAdd(TWZ_ADDR, ET_ADDR+i*SHARES, ET_ADDR+i*SHARES);
-       /*#ifdef DEBUG
-       m = collapse(TWZ_ADDR);
-       re = add(r, x);
-       r = collapse(ET_ADDR+j*SHARES);
-       printf("Ajout du résultat du produit %d dans ET[i] = %d : %d =?= %d... %s\n",
-	      m, x, r, re,
-	      (r == re ?"OK":"KO"));
-      displayWorkZone();
-      #endif*/
+      /* On ajoute ce produit au total dans result[i] */
+       secAdd(TWZ_ADDR, result+i*SHARES, result+i*SHARES);
     }
   }
 #ifdef DEBUG
@@ -145,20 +132,21 @@ void matrix_product() {
 #endif
 }
 
-void add_round_key(int round) {
+void add_round_key(byte * addr, int round) {
   int i;
   for(i = 0; i < LINEAR_SIZE*SHARES; i++)
-    CT_ADDR[i] ^= (KEY_ADDR + round*LINEAR_SIZE*SHARES)[i];
+    addr[i] ^= (KEY_ADDR + round*LINEAR_SIZE*SHARES)[i];
 }
 
 void runSecureAES() {
   int i, j;
+  add_round_key(CT_ADDR, 0);
   for(i = 0; i < NB_ROUNDS; i++) {
-    add_round_key(i);
-    apply_sbox();
-    matrix_product();
+    apply_sbox(CT_ADDR);
+    matrix_product(CT_ADDR, ET_ADDR);
+    add_round_key(ET_ADDR, i+1);
     for(j = 0; j < SHARES*LINEAR_SIZE; j++)
-      CT_ADDR[j] = ET_ADDR[j];
+       CT_ADDR[j] = ET_ADDR[j];
   }
 }
 
--- test_secureAESlike.c
+++ test_secureAESlike.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#include "aesLike.h"
 #include "secureAESlike.h"
+#include "sbox_tools.h"
 
 int main() {
-  int i;
+  int i,j;
   byte lin[2][2];
-  byte sbox[256];
-  byte x[2], y[2], e[2], key[4];
-  int test = 1;
+  byte sbox[256], rpool[256], polySbox[256];
+  byte x[2], y[2], e[2], key[6];
+  int test = 1, test_all;
+
+  /* First test */
   lin[0][0] = 0x03; lin[0][1] = 0x04;
   lin[1][0] = 0x05; lin[1][1] = 0x06;
-  /* key[0] = 0x00; key[1] = 0x00; key[2] = 0x00; key[3] = 0x00; */
-  key[0] = 0x0b; key[1] = 0x0b; key[2] = 0x0b; key[3] = 0x0b;
+  key[0] = 0x00; key[1] = 0x01; key[2] = 0x02; key[3] = 0x03;
+  key[4] = 0x04; key[5] = 0x05;
+  /* key[0] = 0x0b; key[1] = 0x0b; key[2] = 0x0b; key[3] = 0x0b; */
   x[0] = 1; x[1] = 2;
   for(i = 0; i < 256; i++)
-    sbox[i] = 0;
-  sbox[254] = 0x2;
-  e[0] = 0x1a; e[1] = 0xa4;
-  /* srand((unsigned int) time (NULL)); */
-  srand(100);
+    polySbox[i] = 0;
+  polySbox[254] = 0xfe; polySbox[255] = 0xff; 
+  /* e[0] = 0xb6; e[1] = 0x67; */ 
+  /* e[0] = 0xb4; e[1] = 0x64; */
+  e[0] = 0x54; e[1] = 0x98;
+  srand((unsigned int) time (NULL));
+  /* srand(100); */
 
-  loadSecureAES(lin, sbox);
+  printf("Test version secure...\n");
+  printf("Lin :\n");
+  for(i = 0; i<2; i++) {
+    printf("[");
+    for(j = 0; j < 2; j++)
+      printf("%#2.2x ", lin[i][j]);
+    printf("]\n");
+  }
+  printf("Key : [ ");
+  for(i = 0; i<6; i++)
+    printf("%#2.2x ", key[i]);
+  printf("]\n");
+  printf("Clear :   [ ");
+  for(i = 0; i<2; i++)
+    printf("%#2.2x ", x[i]);
+  printf("]\n");
+  loadSecureAES(lin, polySbox);
   setKey(key);
   secureAES(x, y);
   printf("Result :   [ ");
@@ -34,5 +58,108 @@ int main() {
     test &= e[i] == y[i];
   }
   printf("] ... %s\n", (test?"OK":"KO"));
-  return !test;
+  test_all = test;
+
+  printf("Test version pas secure...\n");
+  buildSbox(polySbox, sbox);
+  printf("Validation de la transformation de Sbox... ");
+  for(i = 0; i < 256; i++) {
+    test &= sbox[i] == evalPolySbox(polySbox, i);
+  }
+  printf("%s\n", test?"OK":"KO");
+  
+    printf("Lin :\n");
+  for(i = 0; i<2; i++) {
+    printf("[");
+    for(j = 0; j < 2; j++)
+      printf("%#2.2x ", lin[i][j]);
+    printf("]\n");
+  }
+  printf("Key : [ ");
+  for(i = 0; i<6; i++)
+    printf("%#2.2x ", key[i]);
+  printf("]\n");
+  printf("Clear :   [ ");
+  for(i = 0; i<2; i++)
+    printf("%#2.2x ", x[i]);
+  printf("]\n");
+  loadAESlike(lin, sbox);
+  loadKey(key);
+  aesLike(x, y);
+  printf("Result :   [ ");
+  for(i = 0; i<2; i++)
+    printf("%#2.2x ", y[i]);
+  printf("]\n");
+  printf("Expected : [ ");
+  for(i = 0; i<2; i++) {
+    printf("%#2.2x ", e[i]);
+    test &= e[i] == y[i];
+  }
+  printf("] ... %s\n", (test?"OK":"KO"));
+  test_all = test;
+  
+  /* /\* 2nd test *\/ */
+  /* /\* Sbox random generation *\/ */
+  printf("Génération de la Sbox... ");
+  for(i=0; i < 256; i++) {
+    rpool[i] = 1;
+  }
+  for(i=0; i < 256; i++) {
+    /* TODO Change rand function */
+    byte candidate = rand();
+    if(rpool[candidate]) {
+      rpool[candidate] = 0;
+      sbox[i] = candidate;
+    } else {
+      i--;
+    }
+  }
+  printf("Fait\n");
+  /* Sbox transformation */
+  printf("Transformation de la Sbox vers une version polynomiale... ");
+  buildPolySbox(sbox, polySbox);
+  printf("Fait\n");
+  test = 1;
+  printf("Validation... ");
+  for(i = 0; i < 256; i++) {
+    test &= sbox[i] == evalPolySbox(polySbox, i);
+  }
+  printf("%s\n", test?"OK":"KO");
+  test_all &= test;
+
+  printf("Chargement des algorithmes\n");
+  printf("\tAES-like\n");
+  loadAESlike(lin, sbox);
+  loadKey(key);
+  printf("\tVersion secure\n");
+  loadSecureAES(lin, polySbox);
+  setKey(key);
+
+  printf("Exécution des algorithmes\n");
+  printf("\tAES-like...");
+  aesLike(x, e);
+  printf("Fait\n");
+  printf("\tResult : [ ");
+  for(i = 0; i<2; i++) {
+    printf("%#2.2x ", e[i]);
+    test &= e[i] == y[i];
+  }
+  printf("]\n");
+  printf("Version secure...");
+  secureAES(x, y);
+  printf("Fait\n");
+  printf("\tResult :   [ ");
+  for(i = 0; i<2; i++)
+    printf("%#2.2x ", y[i]);
+  printf("]\n");
+  printf("Test d'égalité : ");
+  test = 1;
+  for(i = 0; i<2; i++) {
+    test &= e[i] == y[i];
+  }
+  printf("%s\n", test?"OK":"KO");
+  test_all &= test;
+
+  printf("Test Secure AES-like : %s\n", test_all?"ALL OK":"KO");
+  return !test_all;
 }
